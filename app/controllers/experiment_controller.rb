1761bb688500f567ecada370780a814effee677e
Euphe <euphetar@gmail.com>
2014-02-17 15:22:40 +0000

    Initial commit

---
+++ app/controllers/experiment_controller.rb
@@ -0,0 +1,21 @@
+class ExperimentController < ApplicationController
+  
+  def index 
+  end
+
+  def show
+  end
+  
+  def create
+  end
+  
+  def add 
+  end
+  
+  def delete
+  end
+  
+  def edit
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Portfolio::Application.config.secret_key_base = '216d54d62b191d9f4eee7443f1715cc8b43b8797c8a590a3782cb40fa143a6e1aaac37b7aac5fe41605e63d1985fc0e3b30ec2239aaac744ca187e731c496395'
